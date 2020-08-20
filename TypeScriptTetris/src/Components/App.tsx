017af0010ccd965d08e38c176f92b13a689cd1b3
Eric Young <eyoung88@frontier.com>
2020-08-20 11:39:43 -0600

    Begin on the routing of react routing

--- TypeScriptTetris/src/Components/App.tsx
+++ TypeScriptTetris/src/Components/App.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { startGame } from '../Game/app';
+import { HashRouter as Router, Route, NavLink } from "react-router-dom";
+import { GameView } from './GameView';
 
 interface IProps { }
 
@@ -8,29 +9,12 @@ interface IState { }
 export class App extends Component<IProps, IState> {
     render(): React.ReactNode {
         return (
-            <>
-                <h1>This is working from React</h1>
-                <div id="container">
-                    <canvas id="gameCanvas" width="240" height="360"></canvas>
-                    <div id="floatingMessage"></div>
-                </div>
-                <div className="instructions">
-                    <b>Keys:</b>
-                    <ul>
-                        <li>Left Arrow - Move shape left</li>
-                        <li>Right Arrow - Move shape right</li>
-                        <li>Up Arrow - Rotate shape</li>
-                        <li>Down Arrow - Drop shape</li>
-                        <li>P - pause / resume game</li>
-                        <li>F - faster</li>
-                        <li>F2 - start new game</li>
-                    </ul>
-                    <div>Score: <span id="scoreLabel"></span></div>
-                    <div>Level: <span id="levelLabel"></span></div>
-                    <div>Rows: <span id="rowsLabel"></span></div>
-                </div>
-                {startGame()}
-            </>
+            <Router>
+                <NavLink to="/play">LINK</NavLink>
+               <div id="pageRoute">
+                    <Route exact path="/play" component={GameView}/>
+               </div>
+            </Router>
         )
     }
 }
