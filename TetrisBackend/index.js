5fca7b40b385b138b9702d81da803e8e050bdd2f
unknown <code-witch@users.noreply.github.com>
2020-08-24 11:38:58 -0600

    add routes & api for lobbies

--- TetrisBackend/index.js
+++ TetrisBackend/index.js
@@ -1,7 +1,13 @@
 const socketio = require("socket.io");
 const express = require("express");
 const http = require("http");
+const bodyParser = require('body-parser');
 const cors = require("cors");
+const routes = require('./routes/routes');
+
+const urlencodedParser = bodyParser.urlencoded({
+    extended: true
+});
 
 const app = express();
 const server = http.createServer(app);
@@ -11,6 +17,15 @@ const io = new socketio(server, {
     pingTimeout: 2000,
     cookie: false
 });
+
+app.get('/lobby', routes.getLobbies);
+app.get('/lobby/open', routes.getOpenLobbies);
+
+app.post('finish', urlencodedParser, routes.finishGame);
+app.post('/join', urlencodedParser, routes.joinOpenLobby);
+app.post('/makeRoom', urlencodedParser, routes.makeRoom);
+app.post('/remove', routes.removeFinishedGames);
+app.post('/start', urlencodedParser, routes.startGame);
 
 io.on('connection', socket => socket.send("HELLO"));
 
